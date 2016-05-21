7d3c383e78a873c51a9d6be7b41e0178731ba7fc
tiagomartinho <tiago.martinho@xpeppers.com>
2016-05-21 19:52:04 +0200

    adds @IBOutlet to display

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -4,6 +4,8 @@ class CalculatorViewController: UIViewController {
 
     let calculator = Calculator()
 
+    @IBOutlet weak var display: UILabel!
+    
     @IBAction func input(sender: UIButton) {
         calculator.input(sender.titleLabel?.text)
     }
