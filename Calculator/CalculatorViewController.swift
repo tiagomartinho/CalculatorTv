bd27ad6cba947466fefac0a727ec1ec0225b5b89
tiagomartinho <tiago.martinho@xpeppers.com>
2016-05-21 19:53:04 +0200

    updates UI when input is received

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -5,8 +5,13 @@ class CalculatorViewController: UIViewController {
     let calculator = Calculator()
 
     @IBOutlet weak var display: UILabel!
-    
+
     @IBAction func input(sender: UIButton) {
         calculator.input(sender.titleLabel?.text)
+        updateUI()
+    }
+
+    func updateUI() {
+        display.text = "0"
     }
 }
